dc1d3cc69ac098f22f10fc0a7ae367561e3b5fb2
Adam456a <104266798+Adam456a@users.noreply.github.com>
2023-04-30 11:23:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time, random, keyboard
 
 
 while True:
-    random_int = random.randint(000000, 999999)
+    random_int = random.randint(0000, 9999)
     print(random_int)
     time.sleep(0.85)
     if keyboard.is_pressed('a'):
